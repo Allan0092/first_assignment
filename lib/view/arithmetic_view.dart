77d7f69dca6ba5f48bb758b4de0a45f46a5c35bd
allan <allan.gtm92@gmail.com>
2024-11-29 16:54:20 +0545

    finished arithmetic view

--- lib/view/arithmetic_view.dart
+++ lib/view/arithmetic_view.dart
@@ -8,8 +8,93 @@ class ArithmeticView extends StatefulWidget {
 }
 
 class _ArithmeticViewState extends State<ArithmeticView> {
+  final firstController = TextEditingController(text: '0');
+  final secondController = TextEditingController(text: '0');
+  final myKey = GlobalKey<FormState>();
+  int result = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Arithmetic View'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Form(
+          key: myKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: firstController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: 'Enter first number',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter first no';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: secondController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: 'Enter second number',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter second no';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Result: $result',
+                style: const TextStyle(fontSize: 30),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (myKey.currentState!.validate()) {
+                      setState(() {
+                        result = int.parse(firstController.text) +
+                            int.parse(secondController.text);
+                      });
+                    }
+                  },
+                  child: const Text('Addition'),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (myKey.currentState!.validate()) {
+                      setState(() {
+                        result = int.parse(firstController.text) -
+                            int.parse(secondController.text);
+                      });
+                    }
+                  },
+                  child: const Text('Subtraction'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
