19826755f3ade50e2b4f6978281828fdf8645d65
allan <allan.gtm92@gmail.com>
2024-11-29 16:11:29 +0545

    dashboard commplete

--- lib/view/dashboard.dart
+++ lib/view/dashboard.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
